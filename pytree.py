be55b86850994c6455ad3c65526019cc2462e4d5
Arindrima Datta <arindrima@MacBook-Pro.local>
2016-10-02 17:40:09 -0400

    another attempt

--- pytree.py
+++ pytree.py
@@ -1,44 +1,58 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys
 import os
-import string
-
-intset = set('1234567890')
+import re
 
 
-def func(x):
-    x = x.lower()
-    for (i, key) in enumerate(x):
-        if key in string.ascii_lowercase or key in intset:
-            return x[i:]
+def sortkeys(s):
+    return re.sub('[^A-Za-z0-9]+', '', s).lower()
 
 
-def dfsTree(curPath, prefix):
-    files = [x for x in os.listdir(curPath) if x[0] != '.']
-    files = sorted(files, key=func)
-    dirN, fileN = 0, 0
-    for i, fname in enumerate(files):
-        if i < len(files) - 1:
-            curPrefix, subdirPrefix = "├── ", "│   "
+def print_tree(path, indent="", lst=[]):
+    files = os.listdir(path)
+    files = list(filter(lambda f: not f.startswith('.'), files))
+    files = sorted(files, key=sortkeys)
+    # files = sorted(files)
+
+    for i in range(0, len(files)):
+        fullpath = path + "/" + files[i]
+        lst.append(fullpath)
+        if (i == len(files) - 1):
+            print(indent + '└── ' + files[i])
+
         else:
-            curPrefix, subdirPrefix = "└── ", "    "
-        print(prefix + curPrefix + fname)
-        if os.path.isfile(os.path.join(curPath, fname)):
-            fileN += 1
-        else:
-            dirN += 1
-            tdirN, tfileN = dfsTree(os.path.join(curPath, fname), prefix + subdirPrefix)
-            dirN, fileN = dirN + tdirN, fileN + tfileN
-    return dirN, fileN
+            print(indent + '├── ' + files[i])
 
-
-def tree(path):
-    print(path)
-    dirN, fileN = dfsTree(path, "")
-    print()
-    print(str(dirN) + (" directories, " if dirN != 1 else " directorie, ") + str(fileN) + (" files" if fileN != 1 else " files"))
-
+        if os.path.isdir(fullpath):
+            if (i == len(files) - 1):
+                print_tree(fullpath, indent + '    ')
+            else:
+                print_tree(fullpath, indent + '│   ')
+    return lst
 
 if __name__ == '__main__':
-    path = sys.argv[1] if len(sys.argv) > 1 else "."
-    tree(path)
+    if len(sys.argv) == 1:
+        dir = '.'
+    if len(sys.argv) == 2:
+        dir = sys.argv[1]
+    elif len(sys.argv) > 2:
+        print("Usage: %s [path]" % sys.argv[0])
+        sys.exit(0)
+
+    if not os.path.isdir(dir):
+        print("E: that is not a valid path")
+        sys.exit(0)
+
+    print(dir)
+    k = print_tree(dir)
+    print
+
+    d1, f1 = 0, 0
+    for i in k:
+        if os.path.isdir(i):
+            d1 += 1
+        else:
+            f1 += 1
+    print("\n" + str(d1) + " directories, " + str(f1) + " files")  # , k, "list"
