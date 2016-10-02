f536d5fd6520fe5ae6c68a05fa64907c8632c467
Arindrima Datta <arindrima@MacBook-Pro.local>
2016-10-02 17:38:53 -0400

    dfs tree

--- pytree.py
+++ pytree.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
-import subprocess
 import sys
+import os
+import string
+
+intset = set('1234567890')
 
 
-# YOUR CODE GOES 
+def func(x):
+    x = x.lower()
+    for (i, key) in enumerate(x):
+        if key in string.ascii_lowercase or key in intset:
+            return x[i:]
+
+
+def dfsTree(curPath, prefix):
+    files = [x for x in os.listdir(curPath) if x[0] != '.']
+    files = sorted(files, key=func)
+    dirN, fileN = 0, 0
+    for i, fname in enumerate(files):
+        if i < len(files) - 1:
+            curPrefix, subdirPrefix = "├── ", "│   "
+        else:
+            curPrefix, subdirPrefix = "└── ", "    "
+        print(prefix + curPrefix + fname)
+        if os.path.isfile(os.path.join(curPath, fname)):
+            fileN += 1
+        else:
+            dirN += 1
+            tdirN, tfileN = dfsTree(os.path.join(curPath, fname), prefix + subdirPrefix)
+            dirN, fileN = dirN + tdirN, fileN + tfileN
+    return dirN, fileN
+
+
+def tree(path):
+    print(path)
+    dirN, fileN = dfsTree(path, "")
+    print()
+    print(str(dirN) + (" directories, " if dirN != 1 else " directorie, ") + str(fileN) + (" files" if fileN != 1 else " files"))
 
 
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    path = sys.argv[1] if len(sys.argv) > 1 else "."
+    tree(path)
